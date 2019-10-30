ad1c7d51b33f337a9505cb575bb56b1dd5db9590
Adriana Fernandes <adrianafernandes.af@gmail.com>
2019-10-30 17:16:13 -0300

    resolucao

---
+++ T8-javascript-III-master/T8-javascript-III-master/01b_ajax_json/index.js
@@ -0,0 +1,3 @@
+// [] iterar sobre o array e mostrar as três imagens usando for()
+// [] iterar sobre o array e mostrar as três imagens usando for...of
+// [] iterar sobre o array e mostrar as três imagens usando forEach
